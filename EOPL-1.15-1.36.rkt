9c2a7a64caa995f4d1d496068c81f3c46159e4a7
luyuhuang <luyuhuang@luyuhuangdeMacBook-Pro.local>
2016-01-07 16:38:16 +0800

    Add 1.28-1.33

--- EOPL-1.15-1.36.rkt
+++ EOPL-1.15-1.36.rkt
@@ -246,3 +246,242 @@
 (flatten '((a b) c (((d)) e)))     ;; ==> '(a b c d e)
 (flatten '(a () (((b))) (() (c)))) ;; ==> '(a b c)
 
+
+;; Exercise1.28 [**](merge loi1 loi2) , where loi1 and loi2 are lists of integers that
+;; are sorted in ascending order, returns sorted list of all the integers in loi1 and loi2.
+
+
+(define merge-ctx
+  (lambda (loi1 loi2 cmp)
+    (cond
+      [(null? loi1)
+       (if (null? loi2) 
+           (cons cmp loi2)
+           (if (< cmp (car loi2))
+               (cons cmp loi2)
+               (cons (car loi2) (cons cmp (cdr loi2)))))]
+       
+      [(null? loi2)
+       (if (null? loi1)
+           (cons cmp loi1)
+           (if (< cmp (car loi1))
+               (cons cmp loi1)
+               (cons (car loi1) (cons cmp (cdr loi1)))))]
+      [else
+       (let ((s1 (car loi1))
+             (s2 (car loi2)))
+         (if (< s1 s2)  ;;  s1 < s2
+             (if (< cmp s1)  ;; cmp < s1 < s2  
+                 (cons cmp (cons s1  (merge-ctx (cdr loi1) (cdr loi2) s2)))
+                 (if (< cmp s2)               ;; s1 < cmp < s2
+                     (cons  s1  (cons cmp (merge-ctx (cdr loi1) (cdr loi2) s2)))
+                     ;; s1 < s2 < cmp
+                     (cons s1 (cons s2 (merge-ctx (cdr loi1) (cdr loi2) cmp)))))
+             ;; s2 < s1
+             (if (< cmp s2) ;; cmp < s2 < s1
+                 (cons  cmp (cons s2 (merge-ctx (cdr loi1) (cdr loi2) s1)))
+                 (if (< cmp s1) ;; s2 < cmp < s1
+                      (cons s2 (cons cmp (merge-ctx (cdr loi1) (cdr loi2) s1)))
+                      ;; s2 < s1 < cmp
+                       (cons s2 (cons s1 (merge-ctx (cdr loi1) (cdr loi2) cmp)))))))])))
+
+(merge-ctx '(1 4) '(1 2 8) 1)   ;; ==> '(1 1 1 2 4 8)
+
+(define merge
+  (lambda (loi1 loi2)
+    (merge-ctx (cdr loi1) loi2 (car loi1))))
+(merge '(1 4) '(1 2 8))  ;; ==> '(1 1 2 4 8)
+(merge '(35 62 81 90 91) '(3 83 85 90)) ;; ==> '(3 35 62 81 83 85 90 90 91)
+
+
+;; Exercise 1.29 [**] (sort loi) retruns a list of the elements of loi in ascending oredr.
+
+(define sort-insert   ;; loi is a list in ascending order
+  (lambda (loi s)
+    (if (null? loi)
+        (cons s loi)
+        (if (< s (car loi))
+            (cons s loi)
+            (cons (car loi) (sort-insert (cdr loi) s))))))
+
+;(sort-insert '(2 5) 3) ;; ==> '(2 3 5)
+;(sort-insert '(2 5) 8) ;; ==> '(2 5 8)
+
+(define sort-ctx
+  (lambda (loi ctx)
+    (if (null? loi)
+        ctx
+        (sort-ctx (cdr loi) (sort-insert ctx (car loi))))))
+;(sort-ctx '(8 2 5 2 3) '()) ;; ==>  '(2 2 3 5 8)
+
+(define sort
+  (lambda (loi)
+    (sort-ctx loi '())))
+
+(sort '(8 2 5 2 3)) ;; ==> '(2 2 3 5 8)
+
+
+;; Exercise 1.30 [**] (sort/predicate pre loi) returns a list of elements sorted by the predicate.
+
+(define sort-insert/predicate   ;; loi is a list in ascending order
+  (lambda (pred loi s)
+    (if (null? loi)
+        (cons s loi)
+        (if (pred s (car loi))
+            (cons s loi)
+            (cons (car loi) (sort-insert/predicate pred (cdr loi) s))))))    
+
+(define sort/predicate
+  (lambda (pred loi)
+    (letrec ((sort/predicate-ctx
+              (lambda (loi ctx)
+                (if (null? loi)
+                    ctx
+                    (sort/predicate-ctx (cdr loi) (sort-insert/predicate pred ctx (car loi)))))))
+             (sort/predicate-ctx loi '()))))
+
+(sort/predicate < '(8 2 5 2 3)) ;; ==>'(2 2 3 5 8)
+(sort/predicate > '(8 2 5 2 3)) ;; ==>'(8 5 3 2 2)
+
+             
+;; Exercise 1.31 [*] Write the following procedures for calculating on a bintree(definition 1.1.7):
+;; leaf and interior-node, which build bintrees, leaf?, which tests whether a bintree is a leaf, and
+;; lson, rson, and contents-of, which extract the components of a node. contents-of should work on both
+;; leaves and interior nodes.
+
+;; Defination 1.1.7 (binary tree)
+;; binary-search-tree ::= Int | (Symbol Binary-search-tree Binary-search-tree)
+
+(define leaf 
+  (lambda (s) ;; s Int
+    (if (number? s)
+        s
+        (error "Tree leaf must be numbere"))))
+
+(define interior-node 
+  (lambda (s lson rson)
+    `(,s ,lson ,rson)))
+
+
+(define leaf? 
+  (lambda (tree)
+    (number? tree)))
+
+(define lson 
+  (lambda (tree)
+   (if (not (leaf? tree))
+       (cadr tree)
+       (error "Error is a leaf"))))
+
+(define rson 
+ (lambda (tree)
+   (if (not (leaf? tree))
+       (caddr tree)
+       (error "Error is a leaf"))))
+
+(define contents-of   ;;; ????
+  (lambda (tree)
+    (car tree)))
+
+(leaf? (leaf 5))
+
+(define tree (interior-node 'red
+      (interior-node 'bar
+        (leaf 26)
+        (leaf 12))
+      (interior-node 'red
+        (leaf 11)
+        (interior-node 'quux
+          (leaf 117)
+          (leaf 14)))))
+tree       
+(leaf? tree)
+(lson tree)
+(rson tree)
+(contents-of tree)   ;; ??
+
+;; Exercise [*] Write a procedure double-tree that kates a bintree, as prepresented
+;; in defination 1.17, and produces another bintree like the otriginal, but with all the 
+;; integers in the leaves doubles.
+
+(define leaf->number 
+  (lambda (leaf)
+    leaf))
+(define tree-sym
+  (lambda (tree)
+    (car tree)))
+
+
+(define double-tree
+  (lambda (tree)
+    (if (leaf? tree)
+        (leaf (+ (leaf->number tree) (leaf->number tree)))
+        (let ((sym (tree-sym tree))
+              (l (lson tree))
+              (r (rson tree)))
+          (interior-node sym (double-tree l) (double-tree r))))))
+
+tree                ;; ==> '(red (bar 26 12) (red 11 (quux 117 14)))
+(double-tree tree)  ;; ==> '(red (bar 52 24) (red 22 (quux 234 28)))
+
+
+
+;; Exercise 1.33 [**]Write a procedure mark-leakes-with-red-depth that takes a bintree (definiton 1.1.7),
+;; and produces a bintree of the same shape as the original, except that in the new tree, each leaf 
+;; contains the integer of nodes between it and the root that contain the symbol red. For example, the expression
+
+;;(mark-leaves-with-red-depth
+;;    (interior-node ’red
+;;      (interior-node ’bar
+;;        (leaf 26)
+;;        (leaf 12))
+;;      (interior-node ’red
+;;        (leaf 11)
+;;        (interior-node ’quux
+;;          (leaf 117)
+;;          (leaf 14))
+;;which is written using the procedures defined in exercise 1.31, should return the bin- tree
+;;  (red
+;;    (bar 1 1)
+;;    (red 2 (quux 2 2)))
+
+
+(define mark-leaves-with-red-depth-ctx
+  (lambda (tree ctx)
+    (if (leaf? tree)
+        (leaf ctx)
+         (let ((sym (tree-sym tree))   ;; tree-sym defined in 1.32
+              (l (lson tree))
+              (r (rson tree)))
+           (if (eqv? sym 'red)
+               (interior-node sym (mark-leaves-with-red-depth-ctx l (+ ctx 1)) (mark-leaves-with-red-depth-ctx r (+ ctx 1)))
+               (interior-node sym (mark-leaves-with-red-depth-ctx l ctx) (mark-leaves-with-red-depth-ctx r ctx)))))))
+
+;(mark-leaves-with-red-depth-ctx tree 0)  ;; ==> (red (bar 1 1) (red 2 (quux 2 2)))
+
+(define mark-leaves-with-red-depth
+  (lambda (tree)
+    (mark-leaves-with-red-depth-ctx tree 0)))
+
+(mark-leaves-with-red-depth
+    (interior-node 'red
+      (interior-node 'bar
+        (leaf 26)
+        (leaf 12))
+      (interior-node 'red
+        (leaf 11)
+        (interior-node 'quux
+          (leaf 117)
+          (leaf 14)))))  ;; ==> (red (bar 1 1) (red 2 (quux 2 2)))
+
+
+;; Exercise 1.34 [***]Write a procedure path that takes an integer n and a binary search tree bst (page 10) 
+;; that contains the integer n, and returns a list of lefts and rights showing how to find the node containing n.
+;; If n is found at the root, it returns the empty list.
+
+;;> (path 17 ’(14 (7 () (12 () ())) (26 (20 (17 () ())
+;;                          ())
+;;                      (31 () ()))))
+;;  (right left left
+
+
