ce6811c56e9e25b1815353243ab087adef54a731
luyuhuang <luyuhuang@luyuhang.local>
2016-01-22 13:01:35 +0800

    Add list-lang

---
+++ chapter3/list-lang.rkt
@@ -0,0 +1,85 @@
+(module lang (lib "eopl.ss" "eopl")
+
+  (require "data-structures.rkt")
+  
+   ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+  
+  (define the-lexical-spec
+    '((whitespace (whitespace) skip)
+      (comment ("%" (arbno (not #\newline))) skip)
+      (identifier
+       (letter (arbno (or letter digit "_" "-" "?")))
+       symbol)
+      (number (digit (arbno digit)) number)
+      (number ("-" digit (arbno digit)) number)))
+     
+  
+  (define the-grammar
+    '((list-gmp 
+       ("list" "(" items ")")   list-exp)
+      (items (number items)    num-exp)
+      (items (identifier items) var-exp)
+      (items ("," items)        items-list)
+      (items ()                 empty-item)
+      
+      ))   
+
+    
+ 
+       
+  
+    
+ 
+  ;;;;;;;;;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;;;;;;;
+  
+  (sllgen:make-define-datatypes the-lexical-spec the-grammar)
+  
+  (define show-the-datatypes
+    (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+  
+  (define scan&parse
+    (sllgen:make-string-parser the-lexical-spec the-grammar))
+  
+  (define just-scan
+    (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+  
+  
+ ; (display (just-scan "list(x,y,z)"))
+ ; (display (scan&parse "list(5,y,z)"))
+  
+  
+  
+  
+   (define value-of-list
+    (lambda (val)
+      (cases list-gmp val
+        (list-exp (item)
+            (value-of-items item)))))
+  
+  (define value-of-items
+    (lambda (val)
+      (cases items val
+        (num-exp (num items)
+            (let ((secend (value-of-items items)))
+              (cons num secend)))
+        (var-exp (var items)
+           (let ((secend (value-of-items items)))
+              (cons var secend)))
+        (items-list (items)
+             (value-of-items items))
+        (empty-item ()
+            '()))))
+  
+  
+  (define list-val (scan&parse "list(5,y,z)"))
+ ; (display list-val) 
+ ; (value-of-list list-val)
+  (display (value-of-list list-val))
+  
+  
+  
+ 
+  
+
+)
